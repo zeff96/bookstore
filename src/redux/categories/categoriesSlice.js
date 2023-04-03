fbfda31620dbd8921cda83dbc836e558c22f3808
zeff96 <zeffo1975@gmail.com>
2023-04-03 16:04:39 +0300

    Add categories slice to the app.

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  categories: [],
+};
+
+const CategoriesSlice = createSlice({
+  name: "categories",
+  initialState,
+  reducers: {},
+});
