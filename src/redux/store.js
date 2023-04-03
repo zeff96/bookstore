0a6596ff5bb9ad8b5d3b3448f40a800a5fd475f7
zeff96 <zeffo1975@gmail.com>
2023-04-03 15:40:46 +0300

    Add book reducer to the store.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import bookReducer from "./books/booksSlice";
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    books: bookReducer,
+  },
 });
