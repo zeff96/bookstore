9147006ab48dee77ad95737e212bdfd2311fee56
zeff96 <zeffo1975@gmail.com>
2023-04-03 15:37:30 +0300

    Add books slice to the project.

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  books: [],
+};
+const booksSlice = createSlice({
+  name: "books",
+  initialState,
+  reducers: {},
+});
+
+export default booksSlice.reducer;
