8b5c8fdf15b093ade6741bdb69850b0964823bc4
zeff96 <zeffo1975@gmail.com>
2023-04-03 15:59:01 +0300

    Add reducer for removing books.

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -12,6 +12,11 @@ const booksSlice = createSlice({
     addBook: (state, action) => {
       state.books.push(action.payload);
     },
+
+    removeBook: (state, action) => {
+      const index = state.books.find((b) => b.id === action.payload.id);
+      state.books.splice(index, 1);
+    },
   },
 });
 
