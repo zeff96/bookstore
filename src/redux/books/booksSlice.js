617fc8b3f4ee2cbffee7daf4f19d9a0359dab2a8
zeff96 <zeffo1975@gmail.com>
2023-04-04 15:54:15 +0300

    Add hardcoded array object and export book array from the slice.

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,11 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  books: [],
+  books: [
+    {
+      item_id: "item1",
+      title: "The Great Gatsby",
+      author: "John Smith",
+      category: "Fiction",
+    },
+    {
+      item_id: "item2",
+      title: "Anna Karenina",
+      author: "Leo Tolstoy",
+      category: "Fiction",
+    },
+    {
+      item_id: "item3",
+      title: "The Selfish Gene",
+      author: "Richard Dawkins",
+      category: "Nonfiction",
+    },
+  ],
 };
 
 const booksSlice = createSlice({
-  name: 'books',
+  name: "books",
   initialState,
   reducers: {
     addBook: (state, action) => {
@@ -19,5 +38,6 @@ const booksSlice = createSlice({
   },
 });
 
+export const selectedBooks = (state) => state.books.books;
 export const { addBook, removeBook } = booksSlice.actions;
 export default booksSlice.reducer;
