4366ed03a91ef3552febcae4618e4f1ee9bf17e9
zeff96 <zeffo1975@gmail.com>
2023-04-04 16:19:39 +0300

    Add title and author as local state to AddBooks func and import usestate hookfrom react.

--- src/components/AddBooks.js
+++ src/components/AddBooks.js
@@ -1,17 +1,23 @@
 import "./AddBooks.scss";
 import { useDispatch } from "react-redux";
 import { addBook } from "../redux/books/booksSlice";
+import { useState } from "react";
 
-const AddBooks = () => (
-  <div className="add-books">
-    <label htmlFor="title">
-      <input type="text" placeholder="Book title" />
-    </label>
-    <label htmlFor="author">
-      <input type="text" placeholder="Book author" />
-    </label>
-    <button type="button">ADD BOOK</button>
-  </div>
-);
+const AddBooks = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+
+  return (
+    <div className="add-books">
+      <label htmlFor="title">
+        <input type="text" placeholder="Book title" />
+      </label>
+      <label htmlFor="author">
+        <input type="text" placeholder="Book author" />
+      </label>
+      <button type="button">ADD BOOK</button>
+    </div>
+  );
+};
 
 export default AddBooks;
