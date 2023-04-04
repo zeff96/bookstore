d1c86eb4d835194a21baaa88f9515619e8dbd732
zeff96 <zeffo1975@gmail.com>
2023-04-04 15:55:34 +0300

    Add selectedBooks as import from the slice, contains books array from the store.

--- src/components/CreateBooks.js
+++ src/components/CreateBooks.js
@@ -1,33 +1,18 @@
 /* eslint-disable react/prop-types */
-import './CreateBooks.scss';
-import CreateBook from './CreateBook';
+import "./CreateBooks.scss";
+import CreateBook from "./CreateBook";
+import { selectedBooks } from "../redux/books/booksSlice";
 
-const books = [
-  {
-    id: 1,
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-  },
-  {
-    id: 2,
-    title: 'Dune',
-    author: 'Frank Herbert',
-  },
-  {
-    id: 3,
-    title: 'Capital in the Twenty-First Century',
-    author: 'Suzanne Collins',
-  },
-];
-
-const AddBookList = () => (
-  <ul className="book-list">
-    {books.map((book) => (
-      <li key={book.id} className="book-item">
-        <CreateBook book={book} title={book.title} author={book.author} />
-      </li>
-    ))}
-  </ul>
-);
+const AddBookList = () => {
+  return (
+    <ul className="book-list">
+      {books.map((book) => (
+        <li key={book.id} className="book-item">
+          <CreateBook book={book} title={book.title} author={book.author} />
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default AddBookList;
