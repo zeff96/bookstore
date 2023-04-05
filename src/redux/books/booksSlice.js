ac914aad7e9584327fbec7809cee99e871b49761
zeff96 <zeffo1975@gmail.com>
2023-04-05 12:57:15 +0300

    Add axios package to the project.

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,30 +1,30 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
   books: [
     {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
+      item_id: "item1",
+      title: "The Great Gatsby",
+      author: "John Smith",
+      category: "Fiction",
     },
     {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
+      item_id: "item2",
+      title: "Anna Karenina",
+      author: "Leo Tolstoy",
+      category: "Fiction",
     },
     {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
+      item_id: "item3",
+      title: "The Selfish Gene",
+      author: "Richard Dawkins",
+      category: "Nonfiction",
     },
   ],
 };
 
 const booksSlice = createSlice({
-  name: 'books',
+  name: "books",
   initialState,
   reducers: {
     addBook: {
@@ -42,7 +42,7 @@ const booksSlice = createSlice({
 
     removeBook: (state, action) => {
       const index = state.books.findIndex(
-        (book) => book.item_id === action.payload.id,
+        (book) => book.item_id === action.payload.id
       );
       state.books.splice(index, 1);
     },
