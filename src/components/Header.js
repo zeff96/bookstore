30346d207b00965679b54247c444eab8c640323c
zeff96 <zeffo1975@gmail.com>
2023-04-06 11:33:46 +0300

    Add unit measurements, font styles and color to books navlink.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,10 @@ import './Header.scss';
 const Header = () => (
   <div className="header">
     <h1 className="header-title">Bookstore CMS</h1>
-    <NavLink className="nav-item" to="/">
+    <NavLink className="books" to="/">
       BOOKS
     </NavLink>
-    <NavLink className="nav-item" to="/categories">
+    <NavLink className="categories" to="/categories">
       CATEGORIES
     </NavLink>
   </div>
