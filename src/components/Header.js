6ff29790444991c0ffd6169d133fe7f6fbf67802
zeff96 <zeffo1975@gmail.com>
2023-04-06 11:26:14 +0300

    Add unit measurements, font styles and color property to header title.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
-import { NavLink } from "react-router-dom";
-import "./Header.scss";
+import { NavLink } from 'react-router-dom';
+import './Header.scss';
 
 const Header = () => (
   <div className="header">
