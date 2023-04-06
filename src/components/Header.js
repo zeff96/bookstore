17c267b5f4f9d311e9801870e9a20a2e60aca7fd
zeff96 <zeffo1975@gmail.com>
2023-04-06 11:19:42 +0300

    Add class attribute to header children.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
-import { NavLink } from 'react-router-dom';
-import './Header.scss';
+import { NavLink } from "react-router-dom";
+import "./Header.scss";
 
 const Header = () => (
   <div className="header">
-    <h1>Bookstore CMS</h1>
-    <NavLink to="/">BOOKS</NavLink>
-    <NavLink to="/categories">CATEGORIES</NavLink>
+    <h1 className="header-title">Bookstore CMS</h1>
+    <NavLink className="nav-item" to="/">
+      BOOKS
+    </NavLink>
+    <NavLink className="nav-item" to="/categories">
+      CATEGORIES
+    </NavLink>
   </div>
 );
 
