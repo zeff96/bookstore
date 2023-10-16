79aecbd66c9cbb41bb7fde1d93cfc019e6003582
zeff96 <zeffo1975@gmail.com>
2023-10-16 22:07:02 +0300

    Add base api service definition

---
+++ src/redux/books/apiSlice/apiSlice.js
@@ -0,0 +1,10 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const appId = 'ao29KDXb9yD3X6fN3lgO';
+
+const url = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${appId}`;
+
+const apiSlice = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: url }),
+  endpoints: () => ({}),
+});
