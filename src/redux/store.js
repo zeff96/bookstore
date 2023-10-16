4873a362cf15d858877f93f91bfb22f143a90755
zeff96 <zeffo1975@gmail.com>
2023-10-16 22:32:42 +0300

    Remove unused redux hook

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import apiSlice from './books/apiSlice/apiSlice';
 import categoryReducer from './categories/categoriesSlice';
 
