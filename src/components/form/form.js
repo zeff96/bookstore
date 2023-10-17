e8c88d7972bc0407ad17e6a5e6d6e44ca6cbb451
zeff96 <zeffo1975@gmail.com>
2023-10-17 06:47:11 +0300

    Add mutation query for adding books to the form for creating a new book

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,54 +1,62 @@
 import { useState } from 'react';
 import { nanoid } from '@reduxjs/toolkit';
-import { useAppDispatch } from '../../hooks/hooks';
-import { setBooksAsync } from '../../redux/books/bookSlice';
+import { useAddBookMutation } from '../../redux/books/bookSlice';
 
 export default function Form() {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
 
-  const dispatch = useAppDispatch();
+  const [addBook, { isLoading }] = useAddBookMutation();
 
-  const handleSubmit = (e) => {
+  const canSave = [title, author].every(Boolean) && !isLoading;
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
     const book = {
       item_id: nanoid(),
       title,
       author,
       category: 'action',
     };
-    dispatch(setBooksAsync(book));
 
-    setTitle('');
-    setAuthor('');
+    if (canSave) {
+      try {
+        await addBook(book).unwrap();
+        setTitle('');
+        setAuthor('');
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
 
   return (
-    <form className="row gap-2" onSubmit={handleSubmit}>
-      <div className="col-md-5 col-lg-6">
+    <form className='row gap-2' onSubmit={handleSubmit}>
+      <div className='col-md-5 col-lg-6'>
         <input
-          type="text"
+          type='text'
           value={title}
-          placeholder="Book title"
-          className="form-control"
+          placeholder='Book title'
+          className='form-control'
           onChange={(e) => {
             setTitle(e.target.value);
           }}
         />
       </div>
-      <div className="col-md-3 col-lg-3">
+      <div className='col-md-3 col-lg-3'>
         <input
-          type="text"
+          type='text'
           value={author}
-          placeholder="Book author"
-          className="form-control"
+          placeholder='Book author'
+          className='form-control'
           onChange={(e) => {
             setAuthor(e.target.value);
           }}
         />
       </div>
-      <div className="col-md-3 col-lg-2">
-        <button className="btn btn-primary" type="submit">
+      <div className='col-md-3 col-lg-2'>
+        <button className='btn btn-primary' type='submit'>
           Add Book
         </button>
       </div>
